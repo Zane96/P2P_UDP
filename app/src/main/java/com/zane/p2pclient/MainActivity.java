baf6f940e7f93037aac7be8445aa1803d72dbd38
Zane Xu <wo1996zhi@vip.qq.com>
2017-06-19 00:06:45 +0800

    some change

--- app/src/main/java/com/zane/p2pclient/MainActivity.java
+++ app/src/main/java/com/zane/p2pclient/MainActivity.java
@@ -55,13 +55,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void execCommand(String command) throws IOException {
-        // start the ls command running
-        //String[] args =  new String[]{"sh", "-c", command};
         Runtime runtime = Runtime.getRuntime();
-        Process proc = runtime.exec(command);        //这句话就是shell与高级语言间的调用
-        //如果有参数的话可以用另外一个被重载的exec方法
-        //实际上这样执行时启动了一个子进程,它没有父进程的控制台
-        //也就看不到输出,所以我们需要用输出流来得到shell执行后的输出
+        Process proc = runtime.exec(command);
+
         InputStream inputstream = proc.getInputStream();
         InputStreamReader inputstreamreader = new InputStreamReader(inputstream);
         BufferedReader bufferedreader = new BufferedReader(inputstreamreader);
@@ -74,18 +70,7 @@ public class MainActivity extends AppCompatActivity {
             sb.append('\n');
         }
         Log.i("testshell", sb.toString());
-        //tv.setText(sb.toString());
-        //使用exec执行不会等执行成功以后才返回,它会立即返回
-        //所以在某些情况下是很要命的(比如复制文件的时候)
-        //使用wairFor()可以等待命令执行完成以后才返回
-//        try {
-//            if (proc.waitFor() != 0) {
-//                System.err.println("exit value = " + proc.exitValue());
-//            }
-//        }
-//        catch (InterruptedException e) {
-//            System.err.println(e);
-//        }
+
     }
 
 }
